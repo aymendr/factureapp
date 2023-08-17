2ec98a9471e65d975244cf87a1e41b827e8147e1
Aymen Drira <driraaymen@gmail.com>
2023-08-17 14:52:07 +0400

    first run

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Client } from './client';
-import { ClientService } from './client.service';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ClientService } from './client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public clients: Client[] | undefined;
+export class AppComponent implements OnInit  {
+  public clients: Client[] = [];
   constructor(private clientService: ClientService){}
+  ngOnInit(): void {
+    this.getClients();
+    throw new Error('Method not implemented.');
+  }
 
   public getClients(): void{
     this.clientService.getClients().subscribe(
--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Client } from './client';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 }) 
 export class ClientService {
 
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
   constructor(private http: HttpClient) { }
   
-  public getClients(): Observable<any>{
-    return this.http.get<any>(`${this.apiServerUrl}/client/all`)
+  public getClients(): Observable<Client[]>{
+    return this.http.get<Client[]>(`${this.apiServerUrl}/client/all`);
   }
 
   public addClient(client: Client):Observable<Client>{
